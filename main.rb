cdd97a3cb3b06a056fb3558984ec391069df2061
«Svetlana <svetaisupova@gmail.com>
2021-07-11 20:17:04 +0000

    added cards

--- main.rb
+++ main.rb
@@ -12,11 +12,15 @@ def player_hand(player)
   player.cards.join(', ')
 end
 
-def dealer_hand(player)
-  dealer.cards.join(', ')
+def dealer_hand(dealer)
+  dealer_hand_private = []
+  dealer.cards.each do |card|
+    dealer_hand_private << card.gsub(/./, "*")
+  end
+  dealer_hand_private.join(', ')
 end
 
 2.times { player.cards << deck.take_card }
 puts "Your cards: #{player_hand(player)}"
 2.times { dealer.cards << deck.take_card }
-# puts "Dealer's cards: #{(dealer.cards.count.times}"
+puts "Dealer's cards: #{dealer_hand(dealer)}"
