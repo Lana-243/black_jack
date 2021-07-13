20b26cea358e44a606c2cbd52d18c463ea7df3d7
«Svetlana <svetaisupova@gmail.com>
2021-07-13 21:32:16 +0000

    added ending

--- game.rb
+++ game.rb
@@ -19,26 +19,45 @@ class Game
     second_round
   end
 
-def player_hand(player)
+def player_hand
   @player.cards.join(', ')
 end
 
-def dealer_hand(dealer)
+def dealer_hand_private
   dealer_hand_private = []
-  dealer.cards.each do |card|
+  @dealer.cards.each do |card|
     dealer_hand_private << card.gsub(/./, "*")
   end
   dealer_hand_private.join(', ')
 end
 
+def dealer_hand_open
+  @dealer.cards.join(', ')
+end
+
 def start_game
-  @player.cards << @deck.take_card
-  @player.cards << @deck.take_card
-  puts "Your cards: #{player_hand(@player)}"
-  puts "You have #{hand_points(@player.cards)} points"
-  2.times { @dealer.cards << @deck.take_card }
-  puts "Dealer's cards: #{dealer_hand(@dealer)}"
+  #twice?
+  add_card(@player)
+  add_card(@player)
+  player_card_info
+  add_card(@dealer)
+  add_card(@dealer)
+  dealer_card_info
   bank_add
+end
+
+def player_card_info
+  puts "Your cards: #{player_hand}"
+  puts "You have #{hand_points(@player.cards)} points"
+end
+
+def dealer_card_info
+  puts "Dealer's cards: #{dealer_hand_open}"
+  puts "Dealer has #{hand_points(@dealer.cards)} points"
+end
+
+def add_card(person)
+  person.cards << @deck.take_card
 end
 
 def bank_add(bank)
@@ -47,40 +66,79 @@ def bank_add(bank)
   @bank += 20
 end
 
+def end_game?
+  (@player.cards.length == 3) && (@player.cards.length == 3)
+end
+
 def player_turn
-  player_turn! if @player.cards.length <= 3
+  if end_game?
+    the_end
+  else
+    player_turn!
+  end  
 end
 
 def player_turn!
   puts 'Press 1 if you want to stand'
   puts 'Press 2 if you want to hit'
-  puts 'Press 3 if you want to fold'
+  puts 'Press 3 if you want to finish'
   answer = gets.chomp
   case answer
     when '1'
       dealer_turn
     when '2'
-      
+      add_card(@player)
+      player_card_info
+      dealer_turn
     when '3'
-      
+      the_end
     else
     #raise error
   end
   
+  def the_end
+    puts 'The game is over!'
+    player_card_info
+    dealer_card_info
+  end
+  
   def dealer_turn
+    if end_game?
+      the_end
+    else
+      dealer_turn!
+    end
+  end  
+  
+  def dealer_turn!
     if hand_points(@dealer.cards) >= 17
       player_turn
     else
-      
+      add_card(@dealer)
     end
   end
 
-  def bust?(points)
-    points <=21
+  def bust?(person)
+    hand_points(person.cards) > 21
   end
+  
+  def player_wins?
+    
+    #recheck
+    (bust?(@player) == false) && (hand_points(@player.cards) > hand_points(@dealer.cards))
+  end
+  
+  def dealer_wins?
+    (bust?(@dealer) == false) && (hand_points(@dealer.cards) > hand_points(@player.cards))
+  end
+  def draw?
+    (bust?(@dealer) == false) && (hand_points(@dealer.cards) == hand_points(@player.cards))
+  end
+  
 
   def winner?
-    if hand_points(@player.cards) > hand_points(@dealer.cards) 
+    if  hand_points(@player.cards) == hand_points(@dealer.cards) == 21
+      hand_points(@player.cards) > hand_points(@dealer.cards) 
       
     end
   end
